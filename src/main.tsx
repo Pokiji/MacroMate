fd6e140cbef3c3339f56e37a3a181b7971b554dc
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-04-11 13:35:23 -0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Landing/>
     {/* <Home /> */}
+    
   </StrictMode>,
 )
--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const APP_KEY = process.env.NUTRITIONIX_APP_KEY;
 function searchNutritionix(query) {
     const options = {
         method: 'GET',
-        url: `https://trackapi.nutritionix.com/v2/search/instant?query=${encodeURIComponent(query)}`,
+        url: `https://trackapi.nutritionix.com/v2/natural/nutrients?query=${encodeURIComponent(query)}`,
         headers: {
             'x-app-id': APP_ID,
             'x-app-key': APP_KEY
@@ -16,10 +16,15 @@ function searchNutritionix(query) {
 
     request(options, (error, response, body) => {
         if (error) throw new Error(error);
+
+        const food = body.foods[0];
         
-        
+        console.log("Calories:" + food.nf_calories);
+        console.log("Protein:" + food.nf_protein);
+        console.log("Carbohydrates" + food.nf_total_carbohydrates);
+        console.log("Sugar" + food.sugars)
     });
 }
 
 // Example use
-searchNutritionix("apple kirkland");
+searchNutritionix("sprite");
