31198aa407ce102b355cc5c7e31d8955b25f0d88
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-04-11 16:45:11 -0700

    yes

--- src/home.tsx
+++ src/home.tsx
@@ -4,9 +4,6 @@ function Home (){
     return(
         <>
             <div>   
-                <div className='nav'>
-                    fent
-                </div>
                 <div className='main'>
                     <div className="container">
 
