81f367b9a909c56b31e36e4fd42650aebb2ee82a
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-04-11 16:40:45 -0700

    update

--- src/server/macros.ts
+++ src/server/macros.ts
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-const myLicenseKey: string = `
-bV3rEkOKKctj5CzpC9IA3l1pe2nnrQm8OjJvN0VfjTOVTsj5TeGuttP2KsFZ
-ES6FLfAUz40SE4N+dTmdMitrQhJ7ZbS4gJwHGxiMZJxiGgXBiD5CLcScJDoH
-0YrlWri01UCEElk4n0VJsSnspEEUqSp3HgQTOpK7qHR2Yh7xW6haEZhk8PIA
-bSkNrUnjOxbzfabCnBEv1BNDoWjU5SQEDfXu216xzQNf8n1fyUzRLg+/XW5U
-S2VmhaMq6D3NyWa/mbFp88L+A+xvZuD5Qo7s2p4ERZZA4qKLYBzBBvLADx47
-vueBuLFPqafJk9xKBgycuD9J7ebLPnInEe9uDhFAIQ==
-U2NhbmJvdFNESwpsb2NhbGhvc3R8bWFjcm9mYWN0b3IueHl6CjE3NDUwMjA3OTkKODM4ODYwNwo4
-`.trim();
-
 interface NutritionixResponse {
     foods: Array<{
         nf_calories: number;
