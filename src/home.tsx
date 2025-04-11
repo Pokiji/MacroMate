879a582797ba028465208b6a8da8128a0ee82c2f
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-04-11 10:51:29 -0700

    home tsx

---
+++ src/home.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './home.css'
+
+function Home (){
+    return(
+        <>
+            <div>
+                <h1>hello</h1>
+            </div>
+        </>
+    )
+   
+}
+
+export default Home
